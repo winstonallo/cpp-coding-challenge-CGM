d734f60640eed2d85899cc866a22596fb41cf753
winstonallo <arthurbiedchar@gmail.com>
2025-01-23 23:59:39 +0000

    add: gtest dep, DeepThought class

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,12 @@
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout
 
-
-class CGM(ConanFile):
+class DeepThought(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps", "CMakeToolchain"
 
     def requirements(self):
-        self.requires("cli/2.2.0")
+        self.requires("gtest/1.15.0")
 
     def layout(self):
         cmake_layout(self)
