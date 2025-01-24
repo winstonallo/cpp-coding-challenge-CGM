cfac58d97259a20971e479aeea2b9707dbeb9098
winstonallo <arthurbiedchar@gmail.com>
2025-01-24 00:06:22 +0000

    add: test ideas

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,1 +1,12 @@
 #include "gtest/gtest.h"
+
+TEST(DeepThoughtAdd, EmptyInput) {}
+TEST(DeepThoughtAdd, QuestionTooLong) {} // > 255 chars until next unquoted question mark
+TEST(DeepThoughtAdd, AnswerTooLong) {}
+TEST(DeepThoughtAdd, MultipleQuestionMarks) {}
+TEST(DeepThoughtAdd, UnclosedQuotes) {}
+TEST(DeepThoughtAdd, AnswerWithoutQuotes) {}
+TEST(DeepThoughtAdd, NoAnswer) {}
+
+TEST(DeepThoughtAsk, EmptyInput) {}
+TEST(DeepThoughtAsk, InputTooLong) {}
