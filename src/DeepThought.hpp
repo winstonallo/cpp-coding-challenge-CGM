1ab28d96a866553b6b138b6a4299e862851ebb16
winstonallo <arthurbiedchar@gmail.com>
2025-01-24 18:53:37 +0000

    add: iteration logic to separate answers from questions

--- src/DeepThought.hpp
+++ src/DeepThought.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "DeepThoughtError.hpp"
+#include <cstddef>
 #include <string>
 #include <utility>
 #include <variant>
@@ -14,7 +15,7 @@ class DeepThought {
     unordered_map<string, vector<string>> _questions;
 
     optional<vector<string>>
-    _lookupQuestion(const string &question) const {
+    lookupQuestion(const string &question) const {
         unordered_map<string, vector<string>>::const_iterator it = _questions.find(question);
 
         if (it == _questions.end()) {
@@ -25,7 +26,7 @@ class DeepThought {
     }
 
     const variant<string, DeepThoughtError>
-    _getQuestion(const string &input) {
+    getQuestion(const string &input) {
         for (size_t idx = 0; idx < input.size(); ++idx) {
 
             if (input[idx] == '"') {
@@ -39,7 +40,7 @@ class DeepThought {
                     DeepThoughtError::Type::QUESTION_TOO_LONG
                 );
             } else if (input[idx] == '?') {
-                return variant<string, DeepThoughtError>(input.substr(0, idx));
+                return variant<string, DeepThoughtError>(input.substr(0, idx + 1));
             }
         }
 
@@ -47,6 +48,52 @@ class DeepThought {
             in_place_type<DeepThoughtError>,
             DeepThoughtError::Type::QUESTION_IN_QUOTES
         );
+    }
+
+    const variant<vector<string>, DeepThoughtError>
+    getAnswers(const string &input) {
+        size_t left = 0;
+        bool inQuotes;
+        vector<string> answers;
+
+        string trimmedInput = input.substr(input.find_first_not_of("\t\n "), input.find_last_not_of("\t\n "));
+
+        if (trimmedInput[0] != '"') {
+            return variant<vector<string>, DeepThoughtError>(
+                in_place_type<DeepThoughtError>,
+                DeepThoughtError::Type::ANSWER_WITHOUT_QUOTES
+            );
+        }
+
+        inQuotes = true;
+
+        for (size_t idx = 1; idx < trimmedInput.size(); ++idx) {
+            // cout << idx << " - " << trimmedInput[idx] << endl;
+            if (trimmedInput[idx] == '"') {
+                if (inQuotes) {
+                    answers.push_back(trimmedInput.substr(left + 1, idx - left - 1));
+                    inQuotes = false;
+                } else {
+                    left = idx;
+                    inQuotes = true;
+                }
+            }
+            if (!inQuotes && string("\t\n ").find_first_of(trimmedInput[idx]) != string::npos) {
+                return variant<vector<string>, DeepThoughtError>(
+                    in_place_type<DeepThoughtError>,
+                    DeepThoughtError::Type::ANSWER_WITHOUT_QUOTES
+                );
+            }
+        }
+
+        if (inQuotes) {
+            return variant<vector<string>, DeepThoughtError>(
+                in_place_type<DeepThoughtError>,
+                DeepThoughtError::Type::UNCLOSED_QUOTES
+            );
+        }
+
+        return variant<vector<string>, DeepThoughtError>(in_place_type<vector<string>>, answers);
     }
 
   public:
@@ -60,7 +107,7 @@ class DeepThought {
 
     const variant<bool, DeepThoughtError>
     processInput(const string &input) {
-        variant<string, DeepThoughtError> questionVariant = _getQuestion(input);
+        variant<string, DeepThoughtError> questionVariant = getQuestion(input);
 
         if (holds_alternative<DeepThoughtError>(questionVariant)) {
             DeepThoughtError &err = get<DeepThoughtError>(questionVariant).addToStackTrace("could not parse question");
@@ -68,16 +115,26 @@ class DeepThought {
         }
 
         const string &question = get<string>(questionVariant);
-
-        optional<vector<string>> answers = _lookupQuestion(question);
-        if (answers.has_value()) {
-            for (string &answer : answers.value()) {
-                cout << answer << '\n';
+        if (input.find_first_not_of("\t\n ", question.size()) == string::npos) {
+            optional<vector<string>> answers = lookupQuestion(question);
+            if (answers.has_value()) {
+                for (string &answer : answers.value()) {
+                    cout << answer << '\n';
+                }
+            } else {
+                cout << "The answer to life, universe and everything is 42." << '\n';
             }
-        } else {
-            cout << "The answer to life, universe and everything is 42." << '\n';
-        }
 
+        } else {
+            variant<vector<string>, DeepThoughtError> result = getAnswers(input.substr(question.size()));
+
+            if (holds_alternative<DeepThoughtError>(result)) {
+                DeepThoughtError &err = get<DeepThoughtError>(result).addToStackTrace("could not parse answers");
+                return variant<bool, DeepThoughtError>(std::move(err));
+            }
+
+            _questions[question] = get<vector<string>>(result);
+        }
         return variant<bool, DeepThoughtError>(true);
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,24 @@ int
 main() {
     DeepThought deepThought;
 
-    variant<bool, DeepThoughtError> result = deepThought.processInput("asaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasaasa?");
+    variant<bool, DeepThoughtError> res1 = deepThought.processInput("Hello, World?");
 
-    if (holds_alternative<DeepThoughtError>(result)) {
-        DeepThoughtError &err = get<DeepThoughtError>(result);
+    if (holds_alternative<DeepThoughtError>(res1)) {
+        DeepThoughtError &err = get<DeepThoughtError>(res1);
+        cerr << "error: " << err.getMessage() << endl;
+    }
+
+    variant<bool, DeepThoughtError> res2 = deepThought.processInput("Hello, World?       \"Hello, Sir!\"    asd    \"How are you today?\"");
+
+    if (holds_alternative<DeepThoughtError>(res2)) {
+        DeepThoughtError &err = get<DeepThoughtError>(res2);
+        cerr << "error: " << err.getMessage() << endl;
+    }
+
+    variant<bool, DeepThoughtError> res3 = deepThought.processInput("Hello, World?");
+
+    if (holds_alternative<DeepThoughtError>(res3)) {
+        DeepThoughtError &err = get<DeepThoughtError>(res3);
         cerr << "error: " << err.getMessage() << endl;
     }
 }
